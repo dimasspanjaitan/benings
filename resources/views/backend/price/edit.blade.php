72e709472124508e398a6d036ff28f22a4847896
dimasspanjaitan <dimasspanjaitan123@gmail.com>
2023-05-20 01:55:55 +0700

    PriceList update (fix field select option product)

--- resources/views/backend/price/edit.blade.php
+++ resources/views/backend/price/edit.blade.php
@@ -11,31 +11,32 @@
 <div class="card">
     <h5 class="card-header">Edit Price Level</h5>
     <div class="card-body">
-      <form method="post" action="{{route('price.update', $data['product_id'])}}">
+      <form method="post" action="{{ route('price.update', $data['product_id']) }}">
         @csrf 
         @method('PATCH')
 
-        <div class="form-group">
+        <div class="form-group" style="padding-right:36px">
           <label for="product_id">Product <span class="text-danger">*</span></label>
-          <select disabled name="product_id" id="product_id" class="form-control">
-              <option value="">--Select any product--</option>
+          <select name="product_id" id="product_id" class="form-control">
               @foreach($data['products'] as $product)
-                  <option value='{{$product->id}}' {{ $product->id == $data['product_id'] ? 'selected' : '' }}>{{$product->name}}</option>
+                  @if ($data['product_id'] == $product->id)
+                    <option value='{{ $product->id }}' selected>{{ $product->name }}</option>
+                  @endif
               @endforeach
           </select>
           @error('product_id')
-            <span class="text-danger">{{$message}}</span>
+            <span class="text-danger">{{ $message }}</span>
           @enderror
         </div>
 
-        <div class="col-12 row">
+        <div class="col-12 row pl-0">
           @foreach ($data['prices'] as $level)
               <div class="col-md-6">
                   <div class="form-group">
-                    <label for="{{ $level->level->name }}" class="col-form-label">{{ $level->level->name }} <span class="text-danger">*</span></label>
-                    <input id="{{ $level->level->id }}" type="number" name="price_{{ $level->level->id }}" placeholder="Enter price"  value="{{ $level->price }}" class="form-control">
+                    <label for="{{  $level->level->name  }}" class="col-form-label">{{  $level->level->name  }} <span class="text-danger">*</span></label>
+                    <input id="{{  $level->level->id  }}" type="number" name="price_{{  $level->level->id  }}" placeholder="Enter price"  value="{{  $level->price  }}" class="form-control">
                     @error("price_".$level->level->id)
-                      <span class="text-danger">{{$message}}</span>
+                      <span class="text-danger">{{ $message }}</span>
                     @enderror
                   </div>
               </div>
