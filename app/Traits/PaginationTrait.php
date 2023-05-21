0c4545bdc399c392a9ca713a9e13bbd65ad36f62
dimasspanjaitan <dimasspanjaitan123@gmail.com>
2023-05-22 01:22:18 +0700

    fix pagination datatable

--- app/Traits/PaginationTrait.php
+++ app/Traits/PaginationTrait.php
@@ -4,26 +4,31 @@ namespace App\Traits;
 use App\Libraries\ApiResponse;
 
 /**
- * Description of StaticResponseTrait
- * @author bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
+ * Pagination Builder
+ * @author dimasspanjaitan <dimasspanjaitan123@gmail.com>
  * @since 
  */
 trait PaginationTrait {
     public function pagination($total){
         $request = request();
         $pages = [];
-        for ($i= (int)$request['_page'] -3; $i <= (int)$request['_page'] + 3 ; $i++) { 
-            if($i >= 1 && $i <= $total/$request['_limit']) array_push($pages,$i);
-        }
+        $page = (int) $request['_page'];
+        $limit = (int) $request['_limit'];
+        $max = (int) ceil($total/$limit);       // ceil -> untuk pembulatan decimal ke atas
+
+        for ($i= $page -3; $i <= $page + 3 ; $i++) { 
+            if($i >= 1 && $i <= $max) array_push($pages,$i);
+        }        
+        $next = $page + 1 > $max ? $page : $page + 1;
 
         $pagination = [
             'total' => $total,
             'pages' => $pages,
-            'prev' => (int) $request['_page'] - 1 <= 1 ? 1 : $request['_page'] - 1,
-            'next' => (int) $request['_page'] + 1 > $total/$request['_limit'] ? $total/$request['_limit'] : $request['_page'] + 1,
-            '_limit' => (int) $request['_limit'],
-            '_page' => (int) $request['_page'],
-            'last' => $total/(int) $request['_limit']
+            'prev' =>  $page - 1 <= 1 ? 1 : $page - 1,
+            'next' => $next,
+            '_limit' => $limit,
+            '_page' => $page,
+            'last' => $max
         ];
 
         return $pagination;
